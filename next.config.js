86301d8f8370a57a77f34ba98a9f38209f9ebfe5
Antonello Simeon <a.simeon87@gmail.com>
2025-09-24 12:35:41 +0000

    Fixing 404 not found

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const isPages = process.env.GITHUB_PAGES === 'true'
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || ''
-const nextConfig = {
+
+module.exports = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
+  // Serve assets from /<REPO>/ when building for Pages
   assetPrefix: isPages && repo ? `/${repo}/` : undefined,
 }
-module.exports = nextConfig
+
